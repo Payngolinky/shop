4e5b012a1de9942118e8f752d61742a663772fdb
Jeffry <jeffryklew@gmail.com>
2021-03-30 07:54:55 -0400

    Convert App to class to store state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+class App extends Component {
+  /**
+   * App constructor initializes state variables
+   */
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  /**
+   * Render UIs for merchant and customer
+   * @returns Complete UIs for merchant and customer in JSX
+   */
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+
+} // class App extends Component
 
 export default App;
