68eeeed5e9c47121d59ecd31363d6f1c8c0504d0
Felix Borjans <felibo@hotmail.de>
2021-03-31 00:27:32 -0700

    can now send AVAX via MetaMask on c-chain

--- src/Metamask.js
+++ src/Metamask.js
@@ -1,21 +1,21 @@
-const Web3 = require("web3");
-
 class MetaMask {
   /**
    * Metamask constructor initializes state variables
    * @constructor
    */
-  ethereum = window.ethereum;
+
   constructor() {
     console.log('MetaMask initializing...');
-    const ethereumButton = document.querySelector('#MetaMaskButton');
+    this.ethereumButton = document.querySelector('#MetaMaskButton');
+    this.sendEthButton = document.querySelector('#MMsendToAddress');
     if(!this.metaMaskInstalled() && this.isMetaMask()){
       console.log('MetaMask is not installed!');
-      ethereumButton.innerHTML('Install Metamask');
+      this.ethereumButton.innerHTML('Install Metamask');
     }
     else{
       console.log('MetaMask is installed');
-      ethereumButton.addEventListener('click', this.linkMetaMaskAccount());
+      this.setupConnect();
+      this.setupSend();
     }
   }
 
@@ -27,10 +27,46 @@ class MetaMask {
     return window.ethereum.isMetaMask;
   }
 
+  setupConnect(){
+    this.ethereumButton.addEventListener('click', () => {this.linkMetaMaskAccount();});
+      window.ethereum.on('accountsChanged', function (accounts) {
+        this.account = accounts[0];
+      // Time to reload your interface with accounts[0]!
+        console.log('The account was changed!')
+      });
+  }
+
+  setupSend(){
+    //Sending Ethereum to an address
+    let myvalue = 0.05e18;
+    let mygasPrice = 470e9;
+    let mygas = 21000;
+    this.sendEthButton.addEventListener('click', () => {
+      window.ethereum
+      .request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: this.account,
+            to: '0x9a57d321084F5b1ac1fCB32230a83Df9EF0AB565',
+            value: myvalue.toString(16),
+            gasPrice: mygasPrice.toString(16),
+            gas: mygas.toString(16),
+          },
+        ],
+      })
+      .then((txHash) => console.log(txHash))
+      .catch((error) => console.error);
+    });
+  }
+
   async linkMetaMaskAccount(){
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
-    this.ethereumButton.innerHTML = account;
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' }).catch((error) => console.error);
+    if(accounts){
+      this.account = accounts[0];
+      this.ethereumButton.innerHTML = this.account;
+      this.sendEthButton.innerHTML = 'Send Coins';
+    }
   }
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './MetaMask.css';
 import App from './App';
 import MetaMask from './Metamask'
 import reportWebVitals from './reportWebVitals';
