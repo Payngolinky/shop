de47ab2e3dafd25dfa88ab9fe0d810a91904bc41
Jeffry <jeffryklew@gmail.com>
2021-03-31 20:40:01 -0400

    Implement ProductGrid.js for displaying multiple ProductCards

---
+++ src/components/Products/ProductGrid.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import ProductCard from './ProductCard';
+
+/**
+ * Defines UI for a grid of product cards
+ * @param {Object[]} products - Array of products to populate the grid with
+ * @returns Grid of product cards in JSX
+ */
+const ProductGrid = ({products}) => {
+  return (
+    <section className="cf">
+      {
+        products.map(product => {
+          return (
+            <div key={product.id} className="fl w-100 w-50-m w-25-l pa3 pa4-l">
+              <ProductCard
+                id={product.id}
+                name={product.name}
+                price={product.price}
+                image_url={product.image_url}
+                desc={product.desc}
+              />
+            </div>
+          );
+        })
+      }
+    </section>
+  );
+} // const ProductGrid = ...
+
+export default ProductGrid;
