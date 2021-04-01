fdd2005e69bbc4a1c920df322e566366d8eaa9e1
Jeffry <jeffryklew@gmail.com>
2021-03-31 20:19:29 -0400

    Defer responsive width to ProductGrid instead of ProductCard

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -11,7 +11,7 @@ import React from 'react';
  */
 const ProductCard = ({id, name, price, image_url, desc}) => {
   return (
-    <article className="br2 ba dark-gray b--black-10 shadow-3 mv4 w-100 w-50-m w-25-l mw5 center">
+    <article className="br2 ba dark-gray b--black-10 shadow-3 mv4 w-100 mw5 center">
       <img
         src={image_url}
         className="db w-100 br2 br--top"
