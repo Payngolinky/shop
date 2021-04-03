fb7ca6a43ccb89837abea248e37352651e3df88a
Jeffry <jeffryklew@gmail.com>
2021-04-03 15:50:08 -0400

    Incorporate MetaMask component and css into MerchantPage.js

--- src/components/MerchantPage/MerchantPage.js
+++ src/components/MerchantPage/MerchantPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './MerchantPage.css';
+import MetaMask from '../MetaMask/MetaMask';
 import ProductGrid from '../Products/ProductGrid';
 import SampleProducts from '../Products/SampleProducts';
 
@@ -9,15 +11,15 @@ import SampleProducts from '../Products/SampleProducts';
  */
 const MerchantPage = () => {
   return (
-    <article className="cf">
+    <article className="cf h-100">
       {/* Wallet on left-hand side (page top for small screens) */}
-      <div className="fl w-100 w-25-l h5 vh-100-l bg-light-blue tc pa3">
-        <h1>Todo: Insert Metamask Wallet UI Here</h1>
+      <div className="fl w-100 w-25-l h5 h-100-l pa3">
+        <MetaMask />
       </div>
 
       {/* Inventory on right-hand side (page bottom for small screens) */}
-      <div className="fl w-100 w-75-l bt b--light-gray bl-l">
-        <h1 className="f3 f2-m f1-l pa3 pa4-ns">
+      <div className="fl w-100 w-75-l bt b--light-gray bl-l bg-inventory">
+        <h1 className="f3 f2-m f1-l tc">
           Manage Inventory
         </h1>
         <ProductGrid products={SampleProducts} />
