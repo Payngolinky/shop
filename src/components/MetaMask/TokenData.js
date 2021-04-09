566fc40ae38c7f759ff4fe795c66d72fdbe930af
Jeffry <jeffryklew@gmail.com>
2021-04-08 21:24:28 -0400

    Implement TokenData.js containing token contract addresses

---
+++ src/components/MetaMask/TokenData.js
@@ -0,0 +1,22 @@
+/**
+ * TokenData contains token contract addresses of selected tokens
+ */
+const TokenData = [
+  {
+    'symbol': 'DAI',
+    'address': '0x34B6C87bb59Eb37EFe35C8d594a234Cd8C654D50',
+    'chainId': '0xa869'
+  },
+  {
+    'symbol': 'PNG',
+    'address': '0x83080D4b5fC60e22dFFA8d14AD3BB41Dde48F199',
+    'chainId': '0xa869'
+  },
+  {
+    'symbol': 'LINK',
+    'address': '0x2F0708E5FB96fd1E9F21eAbAd06EE5F337586A02',
+    'chainId': '0xa869'
+  }
+];
+
+export default TokenData;
