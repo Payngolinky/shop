53da244a7bbbe296e54a12ca90b0c05c278e3160
Jeffry <jeffryklew@gmail.com>
2021-04-10 08:44:00 -0400

    Update TokenData array to contain AVAX, LINK, and ETH

--- src/components/MetaMask/TokenData.js
+++ src/components/MetaMask/TokenData.js
@@ -3,18 +3,18 @@
  */
 const TokenData = [
   {
-    'symbol': 'DAI',
-    'address': '0x34B6C87bb59Eb37EFe35C8d594a234Cd8C654D50',
-    'chainId': '0xa869'
-  },
-  {
-    'symbol': 'PNG',
-    'address': '0x83080D4b5fC60e22dFFA8d14AD3BB41Dde48F199',
+    'symbol': 'AVAX',
+    'address': '',
     'chainId': '0xa869'
   },
   {
     'symbol': 'LINK',
     'address': '0x2F0708E5FB96fd1E9F21eAbAd06EE5F337586A02',
+    'chainId': '0xa869'
+  },
+  {
+    'symbol': 'ETH',
+    'address': '',
     'chainId': '0xa869'
   }
 ];
