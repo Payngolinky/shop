9ef44b603191dd17e817929ee782562fe99f15cd
Jeffry <jeffryklew@gmail.com>
2021-04-11 13:22:59 -0400

    Implement CustomerPage.js, excluding payment functionality

---
+++ src/components/CustomerPage/CustomerPage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Footer from '../Footer/Footer';
+import ProductDescription from './ProductDescription';
+import ProductImageLayout from './ProductImageLayout';
+import SampleProducts from '../Products/SampleProducts';
+
+// Get JSON object containing product details
+const product = SampleProducts[0];
+
+/**
+ * Defines customer UI containing product description and payment info
+ * @returns Customer UI in JSX
+ */
+const CustomerPage = () => {
+  return (
+    <div>
+      <article className="cf h-100">
+        {/* Product images on left-hand side (page top for small screens) */}
+        <div className="fl w-100 w-50-m w-75-l">
+          <ProductImageLayout product={product} />
+        </div>
+
+        {/* Payment UI on right-hand side (page bottom for small screens) */}
+        <div className="fl w-100 w-50-m w-25-l h-100 pa3">
+          <ProductDescription product={product} />
+        </div>
+      </article>
+
+      {/* Footer with social media links */}
+      <Footer />
+    </div>
+  );
+}
+
+export default CustomerPage;
