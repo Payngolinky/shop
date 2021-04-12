eff6b81d9456a58d60659b0843401044b2ab96fd
Jeffry <jeffryklew@gmail.com>
2021-04-11 22:30:37 -0400

    Remove state for token balances from TokenBalanceList.js

--- src/components/TokenBalance/TokenBalanceList.js
+++ src/components/TokenBalance/TokenBalanceList.js
@@ -1,49 +1,14 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 
 import TokenBalance from './TokenBalance';
 import TokenData from './TokenData';
 
 /**
- * String constant representing a balance of 0.0000
- */
-const zeroBalance = '0.0000';
-
-/**
- * Array containing default (zero) balances for each token in TokenData
- */
-let defaultBalances = {};
-TokenData.forEach(data => {
-  defaultBalances[data.symbol] = zeroBalance;
-});
-
-/**
- * Displays the balances of selected tokens in MetaMask wallet
+ * Displays the balances of selected tokens in Felix's MetaMask wallet/contract
  * @param {Object} provider - Web3Provider provider wrapper
  * @returns Multiple TokenBalance UI elements, each displaying a token balance
  */
 const TokenBalanceList = ({ provider }) => {
-  // Balance strings of selected tokens in MetaMask Wallet
-  const [tokenBalances, setTokenBalances] = useState(defaultBalances);
-
-  /**
-   * Update tokenBalances if changed for input token given by symbol
-   * @param {string} symbol - String containing token name
-   * @param {string} balance - New balance for given token
-   */
-  const updateBalance = useCallback((symbol, balance) => {
-    // Copy current state for all tokens in tokenBalances before updating it
-    let copiedBalances = { ...tokenBalances };
-
-    // Balance has changed so update state
-    if (balance !== copiedBalances[symbol]) {
-      // Update copiedBalances with given inputs
-      copiedBalances[symbol] = balance;
-
-      // Finally, update tokenBalances
-      setTokenBalances(copiedBalances);
-    }
-  }, [tokenBalances]);
-
   return (
     <div>
       {
@@ -53,8 +18,6 @@ const TokenBalanceList = ({ provider }) => {
               key={data.symbol + '_' + data.chainId}
               symbol={data.symbol}
               address={data.address}
-              balance={tokenBalances[data.symbol]}
-              updateBalance={updateBalance}
               provider={provider}
             />
           );
